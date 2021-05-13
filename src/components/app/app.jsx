b5a69da49d69aa3b59ea15112b7beb1ca3a1bf8a
Vlad <beloysovvlad@yandex.ru>
2021-05-13 16:04:44 +0400

    structure

---
+++ src/components/app/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './app.scss';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="main">
+
+                </div>
+                <div className="sidebar">
+
+                </div>
+            </div>
+        );
+    }
+}
+export default App;
